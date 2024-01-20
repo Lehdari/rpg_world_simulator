f4bc6d62721297574454a4344100cde2547b98dd
Lehdari <miika.lehtimaki@gmail.com>
2024-01-20 14:25:05 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Project: Project
+// File: main.cpp
+//
+// Copyright (c) 2024 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    printf("jou\n");
+
+    return 0;
+}
